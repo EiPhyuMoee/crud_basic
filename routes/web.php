442f8c7000c10acac81cdce8b78891a1cec965df
EiPhyu Moe <eiphyumoe@securelinkmm.com>
2023-07-28 13:59:50 +0630

    crud_basic

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('testing');
+    return view('welcome');
 });
 Route::resource('std', StudentController::class);
